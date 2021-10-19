672d8f0e46baaf0c050a75b4c07352f9a2e1aa63
小新 <@LAPTOP-EIESGF9D>
2021-10-19 10:57:57 +0800

    添加项目文件。

---
+++ 源.c
@@ -0,0 +1,25 @@
+/*#include <stdio.h>
+#include<math.h>
+int main(void)
+{
+	double a, b, c;
+	double n, m, q;
+	a = 2;
+	b = 7;
+	c = -5;
+	m = sqrt(b * b - 4 * a * c);
+	n = (-b + m) / 2 * a;
+	q = (-b - m) / 2 * a;
+	printf("%lf%lf", n,q);
+	return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{
+	double r, h, v, PI;
+	PI = 3.14;
+	scanf_s("%lf%lf",&r,&h);
+	v = PI * r * r * h;
+	printf("%.2lf",v);
+	return 0;
+}*/
